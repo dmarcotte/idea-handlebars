2d96a120fb87207bef20b5435e3282232302ee82
Daniel Marcotte <daniel.marcotte@bazaarvoice.com>
2012-02-13 18:32:00 -0800

    Enable native HTML formatting in our templates

---
+++ src/com/dmarcotte/handlebars/format/HbFormattingModelBuilder.java
@@ -0,0 +1,6 @@
+package com.dmarcotte.handlebars.format;
+
+import com.intellij.psi.templateLanguages.SimpleTemplateLanguageFormattingModelBuilder;
+
+public class HbFormattingModelBuilder extends SimpleTemplateLanguageFormattingModelBuilder {
+}
