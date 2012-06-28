64e53d02f300192d3f7eced8a13099949c64e4c8
Daniel Marcotte <dmarcotte@gmail.com>
2012-06-28 15:57:07 -0700

    First stab at making Handlebars injectable into the script tags as is common.
    There's a pretty big issue though: the language injection api only seems to support injecting into a single XmlText node, so our injection ends at the first html tag in our template, which is clearly not awesome.

--- src/com/dmarcotte/handlebars/HbLanguage.java
+++ src/com/dmarcotte/handlebars/HbLanguage.java
@@ -1,9 +1,10 @@
 package com.dmarcotte.handlebars;
 
+import com.intellij.lang.InjectableLanguage;
 import com.intellij.lang.Language;
 import com.intellij.psi.templateLanguages.TemplateLanguage;
 
-public class HbLanguage extends Language implements TemplateLanguage {
+public class HbLanguage extends Language implements TemplateLanguage, InjectableLanguage {
     public static final HbLanguage INSTANCE = new HbLanguage();
 
     public HbLanguage() {
