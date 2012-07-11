c54fbf44a9df454019f71b7faacb72ad26619a08
Daniel Marcotte <dmarcotte@gmail.com>
2012-07-11 14:44:04 -0700

    Beef up parser test suite

--- test/src/com/dmarcotte/handlebars/parsing/HbParserSpecTest.java
+++ test/src/com/dmarcotte/handlebars/parsing/HbParserSpecTest.java
@@ -1,31 +1,14 @@
 package com.dmarcotte.handlebars.parsing;
 
-import com.intellij.openapi.fileTypes.LanguageFileType;
-import com.intellij.openapi.fileTypes.StdFileTypes;
-import com.intellij.testFramework.ParsingTestCase;
-
 /**
- * Java representations of the validations in Handlesbars parser_spec.rb
+ * Java representations of the validations in Handlebars parser_spec.rb
  * (Precise revision: https://github.com/wycats/handlebars.js/blob/932e2970ad29b16d6d6874ad0bfb44b07b4cd765/spec/parser_spec.rb)
  *
  * The tests here should map pretty clearly by name to the `it "does something"` validations in parser_spec.rb.
  *
- * Note ParsingTestCase automatically constructs test cases from the files in test/data/parser,
- * so each .hbs has a corresponding .txt file which represents the expected Psi structure.
+ * See the docs on {@see HbParserTest} for info on how these tests work
  */
-public class HbParserSpecTest extends ParsingTestCase {
-
-    @SuppressWarnings("UnusedDeclaration") // TODO odd... StdFileTypes is not initialized on time if it's not forward declared.  Figure out what's up and remove this hack.
-    private static LanguageFileType html = StdFileTypes.HTML;
-
-    public HbParserSpecTest() {
-        super("parser", "hbs", new HbParseDefinition());
-    }
-
-    @Override
-    protected String getTestDataPath() {
-        return "test/data";
-    }
+public class HbParserSpecTest extends HbParserTest {
 
     public void testSimpleMustaches() { doTest(true); }
     public void testMustachesWithPaths() { doTest(true); }
---
+++ test/src/com/dmarcotte/handlebars/parsing/HbParserTest.java
@@ -0,0 +1,35 @@
+package com.dmarcotte.handlebars.parsing;
+
+import com.intellij.openapi.fileTypes.LanguageFileType;
+import com.intellij.openapi.fileTypes.StdFileTypes;
+import com.intellij.testFramework.ParsingTestCase;
+
+/**
+ * ParsingTestCase test are created by placing a MyTestName.hbs file in the test/data/parsing directory with the syntax
+ * you would like to validate, and a MyTestName.txt file representing the expected Psi structure.
+ *
+ * You then create a test of the following form to validate the parser (note how the test name corresponds
+ * to the name of the .hbs and .txt files above):
+ *
+ *<pre>{@code
+ *public void testMyTestName() { doTest(true); }
+ *}</pre>
+ *
+ * <b>TIP:</b> if you create a .hbs file without a .txt file, the test will autogenerate .txt file from the current
+ * parser. So, in practice when creating parser tests, you create the .hbs file, run the test, validate that
+ * the .txt represents the desired Psi structure, then call it a day.
+ */
+public abstract class HbParserTest extends ParsingTestCase {
+
+    @SuppressWarnings("UnusedDeclaration") // TODO odd... StdFileTypes is not initialized on time if it's not forward declared.  Figure out what's up and remove this hack.
+    private static LanguageFileType html = StdFileTypes.HTML;
+
+    public HbParserTest() {
+        super("parser", "hbs", new HbParseDefinition());
+    }
+
+    @Override
+    protected String getTestDataPath() {
+        return "test/data";
+    }
+}
