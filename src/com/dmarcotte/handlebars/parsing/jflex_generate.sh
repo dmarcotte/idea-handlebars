9815efb386508e9cc99ad1ed9fed5426181f3942
Daniel Marcotte <dmarcotte@gmail.com>
2012-11-12 07:27:09 -0800

    Allow jflex_generate to be run from any directory
    jflex_generate relied on being run in the directory it lives in in order to find
    handlebars.flex.  Be more friendly by allowing it to run from any directory.

--- src/com/dmarcotte/handlebars/parsing/jflex_generate.sh
+++ src/com/dmarcotte/handlebars/parsing/jflex_generate.sh
@@ -15,8 +15,13 @@ if [ -z $IDEA_SRC_ROOT ]; then
     exit 1
 fi
 
+# ensure we are in this scripts directory
+pushd "$( dirname $0 )"
+
 # thanks to http://code.google.com/p/google-closure-soy/source/browse/trunk/src/net/intellij/plugins/soy/lexer/build-lexer.cmd
 # for help with the command line switches
 ${IDEA_SRC_ROOT}/tools/lexer/jflex-1.4/bin/jflex --charat --nobak --skel \
   ${IDEA_SRC_ROOT}/tools/lexer/idea-flex.skeleton -d . --verbose handlebars.flex \
   || echo '\nERROR: Lexer generation failed.  Check errors above\n  and ensure you provided the correct IDEA source root'
+
+popd
