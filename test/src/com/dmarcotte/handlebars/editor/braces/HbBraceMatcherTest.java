3bf333acc84364487ddc2bc1a63366b1d5b47ccc
Daniel Marcotte <dmarcotte@gmail.com>
2013-03-17 09:27:27 -0700

    Streamline the brace matcher tests

--- test/src/com/dmarcotte/handlebars/editor/braces/HbBraceMatcherTest.java
+++ test/src/com/dmarcotte/handlebars/editor/braces/HbBraceMatcherTest.java
@@ -7,37 +7,54 @@ import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixtureTestCa
 
 public class HbBraceMatcherTest extends LightPlatformCodeInsightFixtureTestCase {
 
+    private static final String ourBraceMatchIndicator = "<brace_match>";
+
     public HbBraceMatcherTest() {
         IdeaTestCase.initPlatformPrefix();
     }
 
     /**
-     * Heavy lifter for our brace match test.  Expects "fileText" to have two special tokens:
-     * <ul>
-     *     <li>&lt;caret&gt; - representing the position of the caret in the text</li>
-     *     <li>&lt;brace_match&gt; - just before the brace which should be highlighted</li>
-     * </ul>
-     *
-     * NOTE: Your &lt;caret&gt; should be placed in or before the brace you want it to highlight; the actual highlighter
-     *  compensates for this, but we don't need to mess with that in this test since we just want to verify the
-     *  matches.  If you get a "java.lang.IndexOutOfBoundsException: Wrong line: -1" error, your caret is probably
-     *  sitting in whitespace rather than a brace (i.e. you have "}}&lt;caret&gt;" rather than "}&lt;caret&gt;}"
+     * Expects "fileText" to have two "&lt;brace_match&gt;" tokens, placed in front of two braces which are
+     * expected to be matched by the built-in brace matching (i.e. when the caret is at one of the &lt;brace_match&gt;
+     * tokens, the brace match subsystem highlights the brace at the other &lt;brace_match&gt;)
+     * <p>
+     * NOTE: the &lt;brace_match&gt; before you close brace should have a bit of whitespace before it to make this
+     *  test work correctly.  For example, have "{{/ foo <brace_match>}}" rather than "{{/foo<brace_match>}}"
      */
     private void doBraceTest(String fileText) {
-        String braceMatchIndicator = "<brace_match>";
+        String textForTest = fileText;
+
+        int firstBracePosition = textForTest.indexOf(ourBraceMatchIndicator);
+        textForTest = textForTest.replaceFirst(ourBraceMatchIndicator, ""); // remove first brace from input
+
+        int secondBracePosition = textForTest.indexOf(ourBraceMatchIndicator);
+        textForTest = textForTest.replaceFirst(ourBraceMatchIndicator, ""); // remove second brace from input
+
+        assertTrue("Should have two \"" + ourBraceMatchIndicator + "\" tokens in fileText.  Given fileText:\n"
+                           + fileText,
+                   firstBracePosition > -1 && secondBracePosition > -1);
+
+        String firstBraceResult = findMatchBraceForBraceAtCaret(textForTest, firstBracePosition, secondBracePosition);
+        assertEquals("Result with caret at first <brace_match>", fileText, firstBraceResult);
+        String secondBraceResult = findMatchBraceForBraceAtCaret(textForTest, secondBracePosition, firstBracePosition);
+        assertEquals("Result with caret at second <brace_match>", fileText, secondBraceResult);
+    }
+
+    /**
+     * Method to do the actual invocation of the brace match subsystem on a given file for a given caret position
+     *
+     * @param fileText the source to test brace matching on
+     * @param caretPosition caret position to compute a matched brace for
+     * @param expectedMatchedBracePosition the expected position of the brace which matches the brace at caretPosition
+     * @return the given file text with the {link #ourBraceMatchIndicator} tokens place where
+     *      the the brace matching subsystem dictated
+     */
+    private String findMatchBraceForBraceAtCaret(String fileText, int caretPosition, int expectedMatchedBracePosition) {
+
         String caretIndicator = "<caret>";
+        String textWithCaret = new StringBuilder(fileText).insert(caretPosition, caretIndicator).toString();
 
-        // compute the expected position of the matched brace (which does not include the chars from the caretIndicator)
-        int expectedMatchedBracePosition = fileText.replace(caretIndicator, "").indexOf(braceMatchIndicator);
-        assertTrue("Input test string must contain \"" + braceMatchIndicator + "\"", expectedMatchedBracePosition > -1);
-
-        // remove the braceMatchIndicator from the string to prepare for the brace matcher,
-        // and capture the position of the caretIndicator
-        String cleanedFileText = fileText.replace(braceMatchIndicator, "");
-        int caretPosition = cleanedFileText.indexOf(caretIndicator);
-        assertTrue("Input test string must contain \"" + caretIndicator + "\"", caretPosition > -1);
-
-        myFixture.configureByText(HbFileType.INSTANCE, cleanedFileText);
+        myFixture.configureByText(HbFileType.INSTANCE, textWithCaret);
 
         boolean caretFirst = expectedMatchedBracePosition > caretPosition;
         int actualBraceMatchPosition
@@ -47,103 +64,79 @@ public class HbBraceMatcherTest extends LightPlatformCodeInsightFixtureTestCase 
 
         // we want to have an easy to read result, so we insert a <brace_match> where
         // BraceMatchingUtil.getMatchedBraceOffset told us it should go.
-        String result = new StringBuilder(cleanedFileText)
-                // note that we need to compensate for the length of the caretIndicator if it comes before the braceMatchIndicator
-                .insert(actualBraceMatchPosition + (caretFirst ? caretIndicator.length() : 0), braceMatchIndicator)
+        String result = new StringBuilder(textWithCaret)
+                // note that we need to compensate for the length of the caretIndicator if it comes before the ourBraceMatchIndicator
+                .insert(actualBraceMatchPosition + (caretFirst ? caretIndicator.length() : 0), ourBraceMatchIndicator)
                 .toString();
 
-        assertEquals(fileText, result);
+        // replace the caret indicator with a ourBraceMatchIndicator so that our result format matches our input format
+        result = result.replace(caretIndicator, ourBraceMatchIndicator);
+
+        return result;
     }
 
     /**
-     * Convenience property for quickly setting up brace match tests.  Note that all the
-     * mustache ids (foo, foo2, bar, etc) are unique so that they can be easily targetted
-     * by string replace functions.
+     * Convenience property for quickly setting up brace match tests.
+     *
+     * Things to note about this text:
+     *    - The braces we want to match have some whitespace around them (this lets them match when the caret is before them)
+     *    - All mustache ids (foo, foo2, bar, etc) are unique so that they can be easily targeted
+     *       by string replace functions.
      */
-    private static String testSource =
-            "{{#foo}}\n" +
-            "    {{bar}}\n" +
-            "    {{#foo2}}\n" +
+    private static String ourTestSource =
+            "{{# foo }}\n" +
+            "    {{ bar }}\n" +
+            "    {{# foo2 }}\n" +
             "        <div>\n" +
-            "            {{^foo3}}\n" +
+            "            {{^ foo3 }}\n" +
             "                Content\n" +
-            "            {{/foo3}}\n" +
+            "            {{/ foo3 }}\n" +
             "        </div>\n" +
-            "        {{{baz}}}\n" +
-            "        {{bat}}\n" +
-            "        {{>partial}}\n" +
-            "    {{/foo2}}\n" +
-            "{{/foo}}";
+            "        {{{ baz }}}\n" +
+            "        {{ bat }}\n" +
+            "        {{> partial }}\n" +
+            "    {{/ foo2 }}\n" +
+            "{{/ foo }}";
 
     public void testSimpleMustache() {
         doBraceTest(
-                testSource.replace("{{bar}}", "<caret>{{bar}}")
-                        .replace("{{bar}}", "{{bar<brace_match>}}")
-        );
-
-        doBraceTest(
-                testSource.replace("{{bar}}", "<brace_match>{{bar}}")
-                        .replace("{{bar}}", "{{bar}<caret>}")
+                ourTestSource.replace("{{ bar }}", "<brace_match>{{ bar }}")
+                        .replace("{{ bar }}", "{{ bar <brace_match>}}")
         );
     }
 
     public void testUnEscapedMustache() {
         doBraceTest(
-                testSource.replace("{{{baz}}}", "<caret>{{{baz}}}")
-                        .replace("{{{baz}}}", "{{{baz<brace_match>}}}")
-        );
-
-        doBraceTest(
-                testSource.replace("{{{baz}}}", "<brace_match>{{{baz}}}")
-                        .replace("{{{baz}}}", "{{{baz}}<caret>}")
+                ourTestSource.replace("{{{ baz }}}", "<brace_match>{{{ baz }}}")
+                        .replace("{{{ baz }}}", "{{{ baz <brace_match>}}}")
         );
     }
 
     public void testPartial() {
         doBraceTest(
-                testSource.replace("{{>partial}}", "<caret>{{>partial}}")
-                        .replace("{{>partial}}", "{{>partial<brace_match>}}")
-        );
-
-        doBraceTest(
-                testSource.replace("{{>partial}}", "<brace_match>{{>partial}}")
-                        .replace("{{>partial}}", "{{>partial}<caret>}")
+                ourTestSource.replace("{{> partial }}", "<brace_match>{{> partial }}")
+                        .replace("{{> partial }}", "{{> partial <brace_match>}}")
         );
     }
 
     public void testBlockMustache() {
         doBraceTest(
-                testSource.replace("{{#foo}}", "<caret>{{#foo}}")
-                        .replace("{{/foo}}", "{{/foo<brace_match>}}")
-        );
-
-        doBraceTest(
-                testSource.replace("{{#foo}}", "<brace_match>{{#foo}}")
-                        .replace("{{/foo}}", "{{/foo}<caret>}")
+                ourTestSource.replace("{{# foo }}", "<brace_match>{{# foo }}")
+                        .replace("{{/ foo }}", "{{/ foo <brace_match>}}")
         );
     }
 
     public void testNestedBlockStache() {
         doBraceTest(
-                testSource.replace("{{#foo2}}", "<caret>{{#foo2}}")
-                        .replace("{{/foo2}}", "{{/foo2<brace_match>}}")
-        );
-
-        doBraceTest(
-                testSource.replace("{{#foo2}}", "<brace_match>{{#foo2}}")
-                        .replace("{{/foo2}}", "{{/foo2}<caret>}")
+                ourTestSource.replace("{{# foo2 }}", "<brace_match>{{# foo2 }}")
+                        .replace("{{/ foo2 }}", "{{/ foo2 <brace_match>}}")
         );
     }
 
     public void testInverseBlockStache() {
         doBraceTest(
-                testSource.replace("{{^foo3}}", "<caret>{{^foo3}}")
-                        .replace("{{/foo3}}", "{{/foo3<brace_match>}}")
-        );
-
-        doBraceTest(
-                testSource.replace("{{^foo3}}", "<brace_match>{{^foo3}}")
-                        .replace("{{/foo3}}", "{{/foo3}<caret>}")
+                ourTestSource.replace("{{^ foo3 }}", "<brace_match>{{^ foo3 }}")
+                        .replace("{{/ foo3 }}", "{{/ foo3 <brace_match>}}")
         );
     }
 }
