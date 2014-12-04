0ba8a3e9ad5b618e6b2be327af2a0557f63ac8dc
Andrey Starovoyt <andrey.starovoyt@jetbrains.com>
2014-12-04 17:00:51 +0300

    handlebars todo indexer WEB-12826
     Conflicts:
    	gen/com/dmarcotte/handlebars/parsing/_HbLexer.java

---
+++ test/src/com/dmarcotte/handlebars/editor/HbTodoIndexTest.java
@@ -0,0 +1,38 @@
+package com.dmarcotte.handlebars.editor;
+
+
+import com.dmarcotte.handlebars.util.HbTestUtils;
+import com.intellij.psi.search.PsiTodoSearchHelper;
+import com.intellij.psi.search.TodoItem;
+import com.intellij.testFramework.LightPlatformCodeInsightTestCase;
+import com.intellij.testFramework.PlatformTestCase;
+import org.jetbrains.annotations.NotNull;
+
+public class HbTodoIndexTest extends LightPlatformCodeInsightTestCase {
+
+  @SuppressWarnings("JUnitTestCaseWithNonTrivialConstructors")
+  public HbTodoIndexTest() {
+    PlatformTestCase.initPlatformLangPrefix();
+  }
+
+  @Override
+  @NotNull
+  protected String getTestDataPath() {
+    return HbTestUtils.BASE_TEST_DATA_PATH + "/todo/";
+  }
+
+  public void testNoTodo() {
+    checkTodoCount(0);
+  }
+
+  public void testFileWithTwoTodo() {
+    checkTodoCount(2);
+  }
+
+
+  private void checkTodoCount(int expectedTodoCount) {
+    configureByFile(getTestName(true) + ".hbs");
+    TodoItem[] items = PsiTodoSearchHelper.SERVICE.getInstance(getProject()).findTodoItems(getFile());
+    assertEquals(expectedTodoCount, items.length);
+  }
+}
