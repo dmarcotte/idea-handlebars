a2a3321ccb12c30f5d871f4866f59fa6ff11b9a5
Daniel Marcotte <dmarcotte@gmail.com>
2015-07-26 13:33:48 -0700

    Small cleanup in HbTestUtils

--- test/src/com/dmarcotte/handlebars/util/HbTestUtils.java
+++ test/src/com/dmarcotte/handlebars/util/HbTestUtils.java
@@ -5,25 +5,27 @@ import com.intellij.openapi.Disposable;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Disposer;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.TestOnly;
 
 import java.io.File;
 
 public class HbTestUtils {
-    /**
-     * The root of the test data directory
-     */
-    public static final String BASE_TEST_DATA_PATH = new File("test", "data").getAbsolutePath();
+  /**
+   * The root of the test data directory
+   */
+  public static final String BASE_TEST_DATA_PATH = new File("test", "data").getAbsolutePath();
 
-    public static void setOpenHtmlAsHandlebars(final boolean value, @NotNull final Project project, @NotNull Disposable parentDisposable) {
-        final boolean oldValue = HbConfig.shouldOpenHtmlAsHandlebars(project);
-        if (oldValue == value) return;
+  @TestOnly
+  public static void setOpenHtmlAsHandlebars(final boolean value, @NotNull final Project project, @NotNull Disposable parentDisposable) {
+    final boolean oldValue = HbConfig.shouldOpenHtmlAsHandlebars(project);
+    if (oldValue == value) return;
 
-        HbConfig.setShouldOpenHtmlAsHandlebars(value, project);
-        Disposer.register(parentDisposable, new Disposable() {
-            @Override
-            public void dispose() {
-                HbConfig.setShouldOpenHtmlAsHandlebars(oldValue, project);
-            }
-        });
-    }
+    HbConfig.setShouldOpenHtmlAsHandlebars(value, project);
+    Disposer.register(parentDisposable, new Disposable() {
+      @Override
+      public void dispose() {
+        HbConfig.setShouldOpenHtmlAsHandlebars(oldValue, project);
+      }
+    });
+  }
 }
