810f00f6969391b9dc3117f882f3bb613b1d355a
Daniel Marcotte <dmarcotte@gmail.com>
2015-07-26 09:55:15 -0700

    Port keyword completion from intellij-plugins

---
+++ test/src/com/dmarcotte/handlebars/completion/HbKeywordCompletionTest.java
@@ -0,0 +1,17 @@
+package com.dmarcotte.handlebars.completion;
+
+import com.dmarcotte.handlebars.file.HbFileType;
+import com.intellij.codeInsight.completion.CompletionType;
+import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixtureTestCase;
+
+public class HbKeywordCompletionTest extends LightPlatformCodeInsightFixtureTestCase {
+  public void doBasicTest(String text, String... expected) {
+    myFixture.configureByText(HbFileType.INSTANCE, text);
+    myFixture.complete(CompletionType.BASIC);
+    assertContainsElements(myFixture.getLookupElementStrings(), expected);
+  }
+
+  public void testSimple() {
+    doBasicTest("{{#<caret>}}", "if", "each");
+  }
+}
